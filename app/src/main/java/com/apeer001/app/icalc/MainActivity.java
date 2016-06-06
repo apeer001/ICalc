8766a78b0aa83567f1e165f9d1de2f8cc70de527
apeer001 <apeer001@ucr.edu>
2016-06-05 19:29:17 -0700

    made layout changes and added functionality

--- app/src/main/java/com/apeer001/app/icalc/MainActivity.java
+++ app/src/main/java/com/apeer001/app/icalc/MainActivity.java
@@ -2,12 +2,40 @@ package com.apeer001.app.icalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Double left = 0.0;
+    Double right = 0.0;
+    EditText resText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resText = (EditText) findViewById(R.id.ResultText);
+
+    }
+
+
+    public void onClick(View view) {
+        Button b = (Button) view;
+        String val = b.getText().toString();
+        String res = resText.getText().toString();
+        if (!val.equals("+") && !val.equals("-") && !val.equals("/") && !val.equals("x")) {
+            if (val.equals(".") && !res.contains(".")) {
+                val = val.concat("0");
+                res = res.concat(val);
+            } else if (res.equals("0") && !val.equals(".")) {
+                res = val;
+            } else {
+                res = res.concat(val);
+            }
+        }
+        // Update text in text field
+        resText.setText(res);
     }
 }
